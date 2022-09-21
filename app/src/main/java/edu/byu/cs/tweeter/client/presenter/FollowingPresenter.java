49fe001f81c6a2419237c6f2774f39a89291007c
CaraHamilton <carahami13@gmail.com>
2022-09-21 11:34:07 -0600

    In class refactoring

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/FollowingPresenter.java
@@ -0,0 +1,70 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+import java.util.List;
+
+import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.client.model.service.FollowService;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class FollowingPresenter {
+
+    private static final int PAGE_SIZE = 10;
+
+    private View view;
+    private FollowService service;
+
+    private User lastFollowee;
+    private boolean hasMorePages;
+    private boolean isLoading = false;
+
+    public interface View {
+        void displayMessage(String message);
+        void setLoadingFooter(Boolean set);
+        void addFollowees(List<User> followees);
+    }
+
+    public FollowingPresenter(View view) {
+        this.view = view;
+        service = new FollowService();
+    }
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+    public boolean hasMorePages() {
+        return hasMorePages;
+    }
+
+    public void loadMoreItems(User user) {
+        isLoading = true;
+        view.setLoadingFooter(true);
+
+        service.loadMoreItems(Cache.getInstance().getCurrUserAuthToken(), user, PAGE_SIZE, lastFollowee, new GetFollowingObserver());
+    }
+
+    private class GetFollowingObserver implements FollowService.GetFollowingObserver {
+        @Override
+        public void addFollowees(List<User> followees, boolean hasMorePages) {
+            isLoading = false;
+            view.setLoadingFooter(false);
+            lastFollowee = (followees.size() > 0) ? followees.get(followees.size() - 1) : null;
+            view.addFollowees(followees);
+            FollowingPresenter.this.hasMorePages = hasMorePages;
+        }
+
+        @Override
+        public void displayErrorMessage(String message) {
+            isLoading = false;
+            view.displayMessage("Failed to get following: " + message);
+            view.setLoadingFooter(false);
+        }
+
+        @Override
+        public void displayError(Exception ex) {
+            isLoading = false;
+            view.displayMessage("Failed to get following because of exception: " + ex.getMessage());
+            view.setLoadingFooter(false);
+        }
+    }
+
+}
